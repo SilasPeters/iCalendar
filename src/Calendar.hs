2551bb18541966834c0dc53e760a420474417835
Luke Bleijenbergh <lukeheeftmail@gmail.com>
2023-11-30 21:28:45 +0100

    redelijk ver

--- src/Calendar.hs
+++ src/Calendar.hs
@@ -5,6 +5,9 @@ import Prelude hiding ((<$), ($>), (<*), (*>), sequence)
 import DateTime
 import GHC.IO.Encoding (BufferCodec(setState))
 import Data.Char (isUpper, isLetter)
+import Data.Functor.Contravariant (Predicate(getPredicate))
+import Data.Maybe
+
 --import qualified Data.List as words
 
 
@@ -32,12 +35,10 @@ import Data.Char (isUpper, isLetter)
 -}
 
 -- Exercise 6
-data Calendar = Calendar { getProdId :: ProdId
+data Calendar = Calendar { getProdId :: String
                          -- , getVersion :: CalendarVersion -- Constant value
                          , getEvents :: [Event]}
     deriving (Eq, Ord, Show)
-
-newtype ProdId = ProdId String deriving (Eq, Ord, Show)
 
 data Event = Event { getDtStamp     :: DateTime
                    , getUid         :: String
@@ -48,13 +49,7 @@ data Event = Event { getDtStamp     :: DateTime
                    , getLocation    :: Maybe String }
     deriving (Eq, Ord, Show)
 
-data Property =      DtStamp      DateTime
-                   | Uid          String
-                   | DtStart      DateTime
-                   | DtEnd        DateTime
-                   | Description  String
-                   | Summary      String
-                   | Location     String
+
 
 --
 ---- Define newtype's to enforce type checks --huh, waarom zou je niet gewoon DateTime & txt kunnen gebruiken?
@@ -74,8 +69,11 @@ data Property =      DtStamp      DateTime
 -- Exercise 7
 data Token = Value String | Prop String | DT DateTime deriving (Eq, Ord, Show)
 
+
+
+
 scanCalendar :: Parser Char [Token]
-scanCalendar = foldr plop [] <$> many parseToken
+scanCalendar = foldr insertToken [] <$> many parseToken
 
 testScanCalendar :: IO()
 testScanCalendar = do
@@ -86,30 +84,131 @@ testScanCalendar = do
 parseToken :: Parser Char Token
 parseToken = choice [parseProp, DT <$> parseDateTime <* token "\n", parseValue]
 
-plop :: Token -> [Token] -> [Token]
-plop v1@(Value s1) (v2@(Value (' ':s2)):ts) = Value (s1++s2) : ts
-plop t ts = t:ts
+insertToken :: Token -> [Token] -> [Token]
+insertToken v1@(Value s1) (v2@(Value (' ':s2)):ts) = Value (s1++s2) : ts
+insertToken t ts = t:ts
 
 parseValue :: Parser Char Token
-parseValue = Value <$> some (satisfy (/= '\n')) <* token "\n" 
+parseValue = Value <$> some (satisfy (/= '\n')) <* token "\n"
 
 parseProp :: Parser Char Token
 parseProp = Prop <$> many (satisfy isUpper) <* symbol ':'
 
-
-
+testparseCalendar :: IO()
+testparseCalendar = do
+  tekst <- readFile "examples//multiline.ics"
+  print $ run parseCalendar $ fromJust $ run scanCalendar tekst
+  return ()
 
 parseCalendar :: Parser Token Calendar
-parseCalendar = undefined
+parseCalendar = toCal <$ symbol (Prop "Start") <* symbol (Value "VCALENDAR")
+                      <*> some parseCalProp <*> some parseEvent
+                      <* symbol (Prop "END") <* symbol (Value "VCALENDAR")
+
+-- nog niet hier checken of hij valide is, 
+toCal :: [CalProp] -> [Event] -> Calendar
+toCal cps = foldr addEvent (foldl insertCalProp zeroCal cps)
+
+insertCalProp :: Calendar -> CalProp -> Calendar
+insertCalProp c cp = case cp of
+  ProdId id -> c{getProdId = id}
+  Version v -> c --DIT IS NOG NIET AF
+
+addEvent :: Event -> Calendar -> Calendar
+addEvent e c = c{getEvents = e:getEvents c}
+
+zeroCal:: Calendar
+zeroCal = Calendar "" []
+
+parseCalProp :: Parser Token CalProp
+parseCalProp  = choice [
+   Version . getString <$ symbol (Prop "VERSION") <*> satisfy isValue
+  ,ProdId . getString <$ symbol (Prop "PRODID") <*> satisfy isValue
+  ]
+
+data CalProp =  ProdId String | Version String
 
 parseEvent:: Parser Token Event
 parseEvent = listToEvent <$> many parseProperty
 
+data Property =      DtStamp      DateTime
+                   | Uid          String
+                   | DtStart      DateTime
+                   | DtEnd        DateTime
+                   | Description  String
+                   | Summary      String
+                   | Location     String
+
+isValue :: Token -> Bool
+isValue (Value _) = True
+isValue _ = False
+
+isDT :: Token -> Bool
+isDT (DT _) = True
+isDT _ = False
+
+getString :: Token -> String
+getString (Value s) = s
+
+getDate :: Token -> DateTime
+getDate (DT dt) = dt
+
+--mogelijk niet compleet
 parseProperty :: Parser Token Property
-parseProperty = undefined
+parseProperty = choice [
+   Uid . getString <$ symbol (Prop "UID") <*> satisfy isValue
+  ,Description . getString <$ symbol (Prop "DESCRIPTION") <*> satisfy isValue
+  ,Summary . getString <$ symbol (Prop "SUMMARY") <*> satisfy isValue
+  ,Location . getString <$ symbol (Prop "LOCATION") <*> satisfy isValue
+  ,DtStamp . getDate <$ symbol (Prop "DTSTAMP") <*> satisfy isDT
+  ,DtStart . getDate <$ symbol (Prop "DTSTART") <*> satisfy isDT
+  ,DtEnd . getDate <$ symbol (Prop "DTEND") <*> satisfy isDT
+  ]
+
+--data Event = Event { getDtStamp     :: DateTime
+--                   , getUid         :: String
+--                   , getDtStart     :: DateTime
+--                   , getDtEnd       :: DateTime
+--                   , getDescription :: Maybe String
+--                   , getSummary     :: Maybe String
+--                   , getLocation    :: Maybe String }
+
+zeroDate :: Date
+zeroDate = Date (Year 0) (Month 0) (Day 0)
+
+zeroTime :: Time
+zeroTime = Time (Hour 0) (Minute 0) (Second 0)
+
+zeroDT :: DateTime
+zeroDT = DateTime zeroDate zeroTime False
+
+zeroEvent :: Event
+zeroEvent = Event zeroDT "" zeroDT zeroDT Nothing Nothing Nothing
 
 listToEvent:: [Property] -> Event
-listToEvent ps = undefined
+listToEvent = foldl insertProp zeroEvent
+
+getPropDate :: Property -> DateTime
+getPropDate (DtStamp dt) = dt
+getPropDate (DtStart dt) = dt
+getPropDate (DtEnd dt) = dt
+
+getPropString (Uid s) = s
+getPropString (Description s) = s
+getPropString (Summary s) = s
+getPropString (Location s) = s
+
+
+insertProp:: Event -> Property -> Event
+insertProp e p = case p of
+  DtStamp      dt -> e{getDtStamp     = getPropDate p}
+  Uid          s -> e{getUid         = getPropString p}
+  DtStart     dt -> e{getDtStart     = getPropDate p}
+  DtEnd       dt -> e{getDtEnd       = getPropDate p}
+  Description  s -> e{getDescription = Just $ getPropString p}
+  Summary      s -> e{getSummary     = Just $ getPropString p}
+  Location     s -> e{getLocation    = Just $ getPropString p}
+
 
 recognizeCalendar :: String -> Maybe Calendar
 recognizeCalendar s = run scanCalendar s >>= run parseCalendar
